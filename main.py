31f03f35ab6df422fac22def4a61bc5239d8303e
gabriel <batistutag190@gmail.com>
2023-03-08 16:23:41 -0300

    adicionado pause, aviso de enter para continuar e função match para escolhas

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
+import os
+import platform
+
 from modules import locadora
 
 if __name__ == '__main__':
     app = locadora.App()
+    userList = []
+    veiculoList = []
     while(True):
+        def pauseAndClear():
+            input('\n\nPressione [ENTER] para continuar...')
+            so = platform.system()
+            if so == 'Linux':
+                os.system('clear')
+            else:
+                os.system('cls')
+
         print('Tabela da Locadora')
-        print('0 - cadastrar veiculo')
-        print('1 - consultar disponibilidade de veículos')
-        print('2 - listar veículos por marca')
-        print('3 - listar veículos por modelo')
-        print('4 - listar veículos por ano')
-        resp = input('Digite sua escolha: ')
+        print('0 - Cadastrar veiculo')
+        print('1 - Consultar disponibilidade de veículos')
+        print('2 - Listar veículos por marca')
+        print('3 - Listar veículos por modelo')
+        print('4 - Listar veículos por ano')
+        print('5 - Criar usuario')
+        resp = int(input('Digite sua escolha: '))
 
-        if resp == 0:
-            app.cadastrar_veiculo()
-        elif resp == 1:
-            app.consultar_disponibilidade_de_veiculos()
-        elif resp == 2:
-            app.listar_veiculos_por_marca()
-        elif resp == 3:
-            app.listar_veiculos_por_modelo()
-        elif resp == 4:
-            app.listar_veiculos_por_ano()
-
-        
+        match (resp):
+            case 0:
+                app.cadastrar_veiculo()
+                pauseAndClear()
+            case 1:
+                app.consultar_disponibilidade_de_veiculos()
+                pauseAndClear()
+            case 2:
+                app.listar_veiculos_por_marca()
+                pauseAndClear()
+            case 3:
+                app.listar_veiculos_por_modelo()
+                pauseAndClear()
+            case 4:
+                app.listar_veiculos_por_ano()
+                pauseAndClear()
+            case 5:
+                client = app.cadastrar_usuario()
+                pauseAndClear()
+            case _:
+                print('quebrou!')
